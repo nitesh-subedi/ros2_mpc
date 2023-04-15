f4bdb46b44b3aa83b206a6feef3c8855ea48b89d
nitesh <074bme624.nitesh@pcampus.edu.np>
2023-04-15 19:32:29 +0545

    global_obstacale_position_created

--- scripts/laser_scan_modelling.py
+++ scripts/laser_scan_modelling.py
@@ -3,7 +3,11 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 import cv2
+from numba import njit
+import math
+from matplotlib import pyplot as plt
 
 
 def convert_laser_scan_to_occupancy_grid(laser_scan_data, angles, map_resolution, map_size):
@@ -53,11 +57,60 @@ class LaserSubscriber(Node):
         return self.laser_data, self.angles
 
 
-def convert_to_map_coordinates(x, y, occ_grid, map_resolution=0.8):
+class OdomSubscriber(Node):
+    def __init__(self):
+        super().__init__('odom_subscriber')
+        self.orientation = None
+        self.position = None
+        self.odom_subscriber = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+
+    def odom_callback(self, msg):
+        self.position = np.array([msg.pose.pose.position.x + 3.0, msg.pose.pose.position.y - 1.0])
+        self.orientation = np.array(euler_from_quaternion(
+            msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z,
+            msg.pose.pose.orientation.w))
+        self.get_logger().info("Data received!")
+
+    def get_states(self):
+        rclpy.spin_once(self)
+        time.sleep(0.1)
+        return self.position, self.orientation
+
+
+def euler_from_quaternion(x, y, z, w):
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z  # in radians
+
+
+@njit
+def convert_to_map_coordinates(occ_grid, map_resolution=0.8):
     map_origin = np.array([occ_grid.shape[0] // 2, occ_grid.shape[1] // 2]) * map_resolution
-    x_meters = - x * map_resolution + map_origin[1]
-    y_meters = - y * map_resolution + map_origin[0]
-    return y_meters, x_meters
+    meter_x = occ_grid.copy()
+    meter_y = occ_grid.copy()
+    for i in range(occ_grid.shape[0]):
+        for j in range(occ_grid.shape[1]):
+            meter_x[i, j] = - j * map_resolution + map_origin[1]
+            meter_y[i, j] = - i * map_resolution + map_origin[0]
+
+    # rot_matrix = np.array([[np.cos(rotation), -np.sin(rotation)],
+    #                        [np.sin(rotation), np.cos(rotation)]])
+    #
+    # x_new = np.dot(rot_matrix, meter_x)
+    # y_new = np.dot(rot_matrix, meter_y)
+
+    return meter_y, meter_x
 
 
 # x_per_start, y_per_start = (100 - msg['data']['start']['x']) / \
@@ -68,19 +121,60 @@ def convert_to_map_coordinates(x, y, occ_grid, map_resolution=0.8):
 #         x_start, y_start = robot.convert_to_map_coordinates(
 #             x_per_start, y_per_start, mode='mapping')
 
+def rotate_image(img, angle):
+    # get image dimensions
+    (h, w) = img.shape[:2]
+
+    # calculate the center of the image
+    center = (w // 2, h // 2)
+
+    # get the rotation matrix for our chosen angle
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+
+    # rotate the image
+    rotated_img = cv2.warpAffine(img, M, (w, h))
+
+    return rotated_img
+
+
+def rotate_coordinates(coordinates, rotation):
+    rot_matrix = np.array([[np.cos(rotation), -np.sin(rotation)],
+                           [np.sin(rotation), np.cos(rotation)]])
+
+    rotated = np.dot(rot_matrix, coordinates)
+
+    return rotated
+
 
 def main(args=None):
     rclpy.init(args=args)
     laser_node = LaserSubscriber()
+    odom_node = OdomSubscriber()
     scale = 5
+    resolution = 0.1
+    size = 2.5
     while rclpy.ok():
         scan_data, angles = laser_node.get_scan()
+        pos, ori = odom_node.get_states()
         if scan_data is None:
             continue
-        occ_grid = 1 - convert_laser_scan_to_occupancy_grid(scan_data, angles, 0.8, 2 * 2)
-        x, y = convert_to_map_coordinates(4, 4, occ_grid=occ_grid)
-        print(x, y)
-        break
+        occ_grid = 1 - convert_laser_scan_to_occupancy_grid(scan_data, angles, resolution, size * 2)
+        occ_grid = cv2.rotate(occ_grid, cv2.ROTATE_180)
+        x, y = convert_to_map_coordinates(occ_grid=occ_grid, map_resolution=resolution)
+        # print(time.time() - tic)
+        obstacles_indices = np.where(occ_grid == 0)
+        obs_x, obs_y = x[obstacles_indices], y[obstacles_indices]
+        obstacle_array = np.array([obs_x, obs_y])
+        rotated_obstacle = rotate_coordinates(obstacle_array, ori[2])
+        rotated_obstacle[0, :] += pos[0]
+        rotated_obstacle[1, :] += pos[1]
+        plt.scatter(rotated_obstacle[0], rotated_obstacle[1])
+        # plt.scatter(obs_x, obs_y)
+        plt.show()
+        pass
+
+        # print(x, y)
+        # pass
         # occ_grid = cv2.rotate(occ_grid, cv2.ROTATE_180)
         # obstacles = np.where(occ_grid == 100)
         # print(occ_grid)
--- scripts/mpc_node.py
+++ scripts/mpc_node.py
@@ -1,75 +1,187 @@
 import casadi
 import numpy as np
+import rclpy
+from rclpy.node import Node
+from numba import njit
+from sensor_msgs.msg import LaserScan
+import time
 
-opti = casadi.Opti()
 
-N = 20
-dt = 0.2
-costmap_size = 2
-resolution = 0.8
+def convert_laser_scan_to_occupancy_grid(laser_scan_data, angles, map_resolution, map_size):
+    # Calculate the size of each cell in the occupancy grid
+    cell_size = map_resolution
+    angle_min = angles[0]
+    angle_max = angles[1]
 
-# Define state variables
-x = opti.variable()
-y = opti.variable()
-th = opti.variable()
-states = casadi.vertcat(x, y, th)
-n_states = states.shape[0]
+    # Calculate the number of cells in the occupancy grid
+    num_cells = int(map_size / cell_size)
 
-# Define control variables
-v = opti.variable()
-w = opti.variable()
-controls = casadi.vertcat(v, w)
-n_controls = controls.shape[0]
+    # Create an empty occupancy grid
+    occupancy_grid = np.zeros((num_cells, num_cells))
 
-dx = v * casadi.cos(th)
-dy = v * casadi.sin(th)
-dth = w
+    # Convert laser scan data to Cartesian coordinates
+    angles = np.linspace(angle_min, angle_max, len(laser_scan_data))
+    x_coords = laser_scan_data * np.cos(angles)
+    y_coords = laser_scan_data * np.sin(angles)
 
-rhs = casadi.vertcat(dx, dy, dth)
+    # Convert Cartesian coordinates to occupancy grid indices
+    x_indices = np.array((x_coords + (map_size / 2)) / cell_size, dtype=int)
+    y_indices = np.array((y_coords + (map_size / 2)) / cell_size, dtype=int)
 
-# Define the mapping function
-f = casadi.Function('f', [states, controls], [rhs])  # Non-linear mapping function
+    # Set occupied cells in the occupancy grid
+    for x, y in zip(x_indices, y_indices):
+        if 0 <= x < num_cells and 0 <= y < num_cells:
+            occupancy_grid[x, y] = 1
 
-# Define state, control and parameter matrices
-X = opti.variable(n_states, N + 1)
-U = opti.variable(n_controls, N)
-P = opti.parameter(2 * n_states)
-# States Integration
-X[:, 0] = P[0:n_states]  # Initial State
-for k in range(N):
-    st = X[:, k]
-    con = U[:, k]
-    f_value = f(st, con)
-    st_next = st + dt * f_value
-    X[:, k + 1] = st_next
-    # opti.subject_to(X[:, k + 1] == st_next)
+    return occupancy_grid
 
-obj = 0
 
-# Defining weighing matrices
-Q = np.eye(n_states, dtype=float)
-Q = Q * 0.5
+class LaserSubscriber(Node):
+    def __init__(self):
+        super().__init__('laser_subscriber')
+        self.angles = None
+        self.laser_data = None
+        self.map_subscriber = self.create_subscription(LaserScan, '/scan', self.laser_callback, 10)
 
-R = np.eye(n_controls, dtype=float)
-R = R * 0.5
+    def laser_callback(self, msg):
+        self.laser_data = np.array(msg.ranges)
+        self.angles = np.array([msg.angle_min, msg.angle_max])
+        self.get_logger().info("Data received!")
 
-for k in range(N):
-    st = X[:, k]
-    con = U[:, k]
-    obj = obj + casadi.mtimes(casadi.mtimes((st - P[n_states:2 * n_states]).T, Q),
-                              (st - P[n_states:2 * n_states])) + casadi.mtimes(casadi.mtimes(con.T, R), con)
+    def get_scan(self):
+        rclpy.spin_once(self)
+        time.sleep(0.1)
+        return self.laser_data, self.angles
 
-opti.minimize(obj)
 
-max_vel = 0.5
-opti.subject_to(U[0, :] <= max_vel)
-opti.subject_to(U[0, :] >= -max_vel)
+@njit
+def convert_to_map_coordinates(occ_grid, map_resolution=0.8):
+    map_origin = np.array([occ_grid.shape[0] // 2, occ_grid.shape[1] // 2]) * map_resolution
+    meter_x = occ_grid.copy()
+    meter_y = occ_grid.copy()
+    for i in range(occ_grid.shape[0]):
+        for j in range(occ_grid.shape[1]):
+            meter_x[i, j] = - j * map_resolution + map_origin[1]
+            meter_y[i, j] = - i * map_resolution + map_origin[0]
 
-max_ang = 0.2
-opti.subject_to(U[1, :] <= max_ang)
-opti.subject_to(U[1, :] >= -max_ang)
+    return meter_y, meter_x
 
-# Define obstacles
-obstacles = opti.parameter(int((costmap_size * 2) / resolution), int((costmap_size * 2) / resolution))
 
-print(obstacles.shape)
+class OptiPlanner:
+    def __init__(self, costmap_size, resolution):
+        self.opti = casadi.Opti()
+        self.N = 20
+        self.dt = 0.2
+        self.costmap_size = 2
+        self.resolution = 0.5
+
+        # Define state variables
+        self.x = self.opti.variable()
+        self.y = self.opti.variable()
+        self.th = self.opti.variable()
+        self.states = casadi.vertcat(self.x, self.y, self.th)
+        self.n_states = self.states.shape[0]
+
+        # Define control variables
+        self.v = self.opti.variable()
+        self.w = self.opti.variable()
+        self.controls = casadi.vertcat(self.v, self.w)
+        self.n_controls = self.controls.shape[0]
+
+        self.dx = self.v * casadi.cos(self.th)
+        self.dy = self.v * casadi.sin(self.th)
+        self.dth = self.w
+
+        self.rhs = casadi.vertcat(self.dx, self.dy, self.dth)
+
+        # Define the mapping function
+        self.f = casadi.Function('f', [self.states, self.controls], [self.rhs])  # Non-linear mapping function
+
+        # Define state, control and parameter matrices
+        self.X = self.opti.variable(self.n_states, self.N + 1)
+        self.U = self.opti.variable(self.n_controls, self.N)
+        self.P = self.opti.parameter(2 * self.n_states)
+        # States Integration
+        self.X[:, 0] = self.P[0:self.n_states]  # Initial State
+        for k in range(self.N):
+            st = self.X[:, k]
+            con = self.U[:, k]
+            f_value = self.f(st, con)
+            st_next = st + self.dt * f_value
+            self.X[:, k + 1] = st_next
+            # self.opti.subject_to(X[:, k + 1] == st_next)
+
+        obj = 0
+
+        # Defining weighing matrices
+        Q = np.eye(self.n_states, dtype=float)
+        Q = Q * 0.5
+
+        R = np.eye(self.n_controls, dtype=float)
+        R = R * 0.5
+
+        for k in range(self.N):
+            st = self.X[:, k]
+            con = self.U[:, k]
+            obj = obj + casadi.mtimes(casadi.mtimes((st - self.P[self.n_states:2 * self.n_states]).T, Q),
+                                      (st - self.P[self.n_states:2 * self.n_states])) + casadi.mtimes(
+                casadi.mtimes(con.T, R), con)
+
+        self.opti.minimize(obj)
+
+        max_vel = 0.5
+        self.opti.subject_to(self.U[0, :] <= max_vel)
+        self.opti.subject_to(self.U[0, :] >= -max_vel)
+
+        max_ang = 0.2
+        self.opti.subject_to(self.U[1, :] <= max_ang)
+        self.opti.subject_to(self.U[1, :] >= -max_ang)
+
+        # Define obstacles
+        obstacles_x = self.opti.parameter(int((costmap_size * 2) / resolution), int((costmap_size * 2) / resolution))
+        obstacles_y = self.opti.parameter(int((costmap_size * 2) / resolution), int((costmap_size * 2) / resolution))
+
+        for i in range(obstacles_x.shape[0]):
+            for j in range(obstacles_x.shape[1]):
+                for k in range(self.N + 1):
+                    distance = np.sqrt((self.X[0, k] - obstacles_x[i, j]) ** 2 + (self.X[1, k] - obstacles_y[i, j]))
+                    self.opti.subject_to(distance > 0.3)
+
+        self.opti.solver('ipopt')
+        self.initial_state = casadi.vertcat(0, 0, 0)
+        self.final_state = casadi.vertcat(2, 0, 0)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    laser_node = LaserSubscriber()
+    resolution = 0.8
+    size = 2
+    planner = OptiPlanner(size, resolution)
+    while rclpy.ok():
+        scan_data, angles = laser_node.get_scan()
+        if scan_data is None:
+            continue
+        occ_grid = 1 - convert_laser_scan_to_occupancy_grid(scan_data, angles, resolution, size * 2)
+        occ_grid = cv2.rotate(occ_grid, cv2.ROTATE_180)
+        tic = time.time()
+        x, y = convert_to_map_coordinates(occ_grid=occ_grid, map_resolution=resolution)
+        print(time.time() - tic)
+        obstacles_indices = np.where(occ_grid == 0)
+        obs_x, obs_y = x[obstacles_indices], y[obstacles_indices]
+
+    # y_obs = np.array([-0.8, -1.6, 1.6, 0.8, -0.8, -0.8])
+    # x_obs = np.array([1.6, 1.6, 0.8, 0.8, 0.8, 0.0])
+    #
+    # x_obs_array = np.ones(obstacles_x.shape) * x_obs[0]
+    # x_obs_array = x_obs_array.ravel()
+    # x_obs_array[:len(x_obs)] = x_obs
+    # x_obs_array = np.reshape(x_obs_array, obstacles_x.shape)
+    # y_obs_array = np.ones(obstacles_y.shape) * y_obs[0]
+    # y_obs_array = y_obs_array.ravel()
+    # y_obs_array[:len(y_obs)] = y_obs
+    # y_obs_array = np.reshape(y_obs_array, obstacles_y.shape)
+
+
+if __name__ == '__main__':
+    main()

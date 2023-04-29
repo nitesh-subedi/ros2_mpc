ae1f8b09a6b69d56e0e5a45ba1730d0c5ed45919
nitesh <074bme624.nitesh@pcampus.edu.np>
2023-04-30 00:27:47 +0545

    obstacle_cost_and_position_cost_tuning_on_progress

--- ros2_mpc/mpc.py
+++ ros2_mpc/mpc.py
@@ -4,16 +4,19 @@ import matplotlib.pyplot as plt
 
 
 class Mpc:
-    def __init__(self, dt, N):
+    def __init__(self, dt, N, cost_factor, costmap_size=2, resolution=0.05, inflation_radius=0.22):
         self.dt = dt
         self.N = N
+        self.inflation_radius = inflation_radius
         self.opti = casadi.Opti()
 
         # Get system function 'f'
         self.f, self.n_states, self.n_controls = self.get_system_function()
 
         # Get decision variables
-        self.X, self.U, self.P = self.get_decision_variables()
+        self.X, self.U, self.P, self.obstacles_x, self.obstacles_y = self.get_decision_variables(
+            costmap_size=costmap_size,
+            resolution=resolution)
 
         # Perform integration using RK4
         self.rk4()
@@ -21,8 +24,9 @@ class Mpc:
         # Perform integration using Euler
         # self.euler_integration()
 
+        obstacles_cost = self.define_obstacles_cost_function(cost_factor=cost_factor)
         # Define cost function
-        self.define_cost_function()
+        self.define_cost_function(obstacles_cost)
 
         # Define constraints
         self.constraints()
@@ -33,10 +37,23 @@ class Mpc:
         # Define solver
         self.opti.solver('ipopt')
 
-    def perform_mpc(self, u0, initial_state=np.array([0, 0, 0]), final_state=np.array([10, 10, 0])):
+    def define_obstacles_cost_function(self, cost_factor):
+        obj = 0
+        for k in range(self.N + 1):
+            for i in range(self.obstacles_x.shape[0]):
+                hxy = casadi.log(((self.X[0, k] - self.obstacles_x[i]) / self.inflation_radius) ** 2 + (
+                        (self.X[1, k] - self.obstacles_y[i]) / self.inflation_radius) ** 2)
+                obj = obj + casadi.exp(cost_factor * casadi.exp(-hxy))
+        return obj
+
+    def perform_mpc(self, u0, initial_state=np.array([0, 0, 0]), final_state=np.array([10, 10, 0]), obstacles_x=None,
+                    obstacles_y=None):
         # Set initial state and parameter value
         self.opti.set_initial(self.U, u0)
         self.opti.set_value(self.P, casadi.vertcat(initial_state, final_state))
+        if obstacles_x is not None and obstacles_y is not None:
+            self.opti.set_value(self.obstacles_x, obstacles_x)
+            self.opti.set_value(self.obstacles_y, obstacles_y)
         # Solve the optimization problem
         sol = self.opti.solve()
         # Extract optimal control
@@ -44,49 +61,44 @@ class Mpc:
         x_opt = sol.value(self.X)
         return x_opt, u_opt
 
-    def obstacle_avoidance_constraints(self):
-        # Define obstacle at (x,y) = (5,3)
-        x_obs = 4
-        y_obs = 7
-        # Define radius of obstacle
-        r_obs = 1
-        # Define obstacle avoidance constraint
-        for k in range(self.N):
-            self.opti.subject_to((self.X[0, k] - x_obs) ** 2 + (self.X[1, k] - y_obs) ** 2 >= r_obs ** 2)
+    # def obstacle_avoidance_constraints(self):
+    #     # Define obstacle at (x,y) = (5,3)
+    #     x_obs = 4
+    #     y_obs = 7
+    #     # Define radius of obstacle
+    #     r_obs = 1
+    #     # Define obstacle avoidance constraint
+    #     for k in range(self.N):
+    #         self.opti.subject_to((self.X[0, k] - x_obs) ** 2 + (self.X[1, k] - y_obs) ** 2 >= r_obs ** 2)
 
     def constraints(self):
         # Define constraints
         self.opti.subject_to(self.opti.bounded(-0.2, self.U[0, :], 0.2))
         self.opti.subject_to(self.opti.bounded(-0.1, self.U[1, :], 0.1))
 
-    def define_cost_function(self):
+    def define_cost_function(self, obstacles_cost):
         # Define cost function
         Q = np.eye(self.n_states, dtype=float)
         Q[0, 0] = 0.0005
         Q[1, 1] = 0.05
-        Q[2, 2] = 0.05
+        Q[2, 2] = 0.5
         obj = 0
         R = np.eye(self.n_controls, dtype=float)
         R = R * 0.005
-        x_obs = 4
-        y_obs = 7
-        r_obs = 1
         for k in range(self.N):
             st = self.X[:, k]
             con = self.U[:, k]
             obj = obj + casadi.mtimes(casadi.mtimes((st - self.P[self.n_states:2 * self.n_states]).T, Q),
                                       (st - self.P[self.n_states:2 * self.n_states])) + casadi.mtimes(
                 casadi.mtimes(con.T, R), con)
-            hxy = casadi.log(((self.X[0, k] - x_obs) / r_obs) ** 2 + ((self.X[1, k] - y_obs) / r_obs) ** 2)
-            obj += casadi.exp(0.1 * casadi.exp(-hxy))
-        self.opti.minimize(obj)
+        self.opti.minimize(obj + obstacles_cost)
 
     def euler_integration(self):
         self.X[:, 0] = self.P[0:self.n_states]
         for k in range(self.N):
             x_next = self.X[:, k] + self.dt * self.f(self.X[:, k], self.U[:, k])
-            # self.X[:, k + 1] = x_next
-            self.opti.subject_to(self.X[:, k + 1] == x_next)
+            self.X[:, k + 1] = x_next
+            # self.opti.subject_to(self.X[:, k + 1] == x_next)
 
     def rk4(self):
         self.X[:, 0] = self.P[0:self.n_states]  # Initial State
@@ -102,12 +114,14 @@ class Mpc:
             # self.X[:, k + 1] = x_next
             self.opti.subject_to(self.X[:, k + 1] == x_next)
 
-    def get_decision_variables(self):
+    def get_decision_variables(self, costmap_size=2, resolution=0.05):
         # Define decision variables
         x = self.opti.variable(self.n_states, self.N + 1)
         u = self.opti.variable(self.n_controls, self.N)
         p = self.opti.parameter(2 * self.n_states)
-        return x, u, p
+        obstacles_x = self.opti.parameter(int((costmap_size * 2) / resolution) * 2)
+        obstacles_y = self.opti.parameter(int((costmap_size * 2) / resolution) * 2)
+        return x, u, p, obstacles_x, obstacles_y
 
     def get_system_function(self):
         # Define number of states
@@ -127,33 +141,3 @@ class Mpc:
         rhs = casadi.vertcat(v * casadi.cos(theta), v * casadi.sin(theta), w)
         f = casadi.Function('f', [states, controls], [rhs], ['input_state', 'control_input'], ['rhs'])
         return f, n_states, n_controls
-
-# def main():
-#     # Define time step
-#     dt = 0.2
-#     # Define prediction horizon
-#     N = 20
-#     # Define initial state
-#     x0 = np.array([0, 0, 0])
-#     # Define final state
-#     xf = np.array([10, 10, 0])
-#     # Create an instance of the MPC class
-#     mpc = MpcClass(dt, N, x0, xf)
-#     count = 0
-#     x_pos = []
-#     u0 = np.zeros((mpc.n_controls, mpc.N))
-#     while np.linalg.norm(x0 - xf) > 0.2 and count < 500:
-#         x, u = mpc.perform_mpc(u0=u0, initial_state=x0, final_state=xf)
-#         x0 = x[:, 1]
-#         u0 = np.concatenate((u[:, 1:], u[:, -1].reshape(2, 1)), axis=1)
-#         count += 1
-#         x_pos.append(x0)
-#         print(x0)
-#
-#     x_pos = np.array(x_pos)
-#     plt.plot(x_pos[:, 0], x_pos[:, 1])
-#     plt.show()
-#
-#
-# if __name__ == '__main__':
-#     main()
--- scripts/test.py
+++ scripts/test.py
@@ -2,29 +2,132 @@ import casadi
 import numpy as np
 import matplotlib.pyplot as plt
 from ros2_mpc.mpc import Mpc
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+import rclpy
+import time
+from ros2_mpc.utils import *
+
+
+class LaserSubscriber(Node):
+    def __init__(self):
+        super().__init__('laser_subscriber')
+        self.angles = None
+        self.laser_data = None
+        self.map_subscriber = self.create_subscription(LaserScan, '/scan', self.laser_callback, 10)
+
+    def laser_callback(self, msg):
+        self.laser_data = np.array(msg.ranges)
+        self.angles = np.array([msg.angle_min, msg.angle_max])
+        self.get_logger().info("Data received!")
+
+    def get_scan(self):
+        rclpy.spin_once(self)
+        time.sleep(0.1)
+        return self.laser_data, self.angles
+
+
+class OdomSubscriber(Node):
+    def __init__(self):
+        super().__init__('odom_subscriber')
+        self.velocities = None
+        self.orientation = None
+        self.position = None
+        self.odom_subscriber = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+
+    def odom_callback(self, msg):
+        self.position = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y]).round(decimals=2)
+        self.orientation = np.array(euler_from_quaternion(
+            msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z,
+            msg.pose.pose.orientation.w)).round(decimals=2)
+        self.velocities = np.array([msg.twist.twist.linear.x, msg.twist.twist.angular.z]).round(decimals=2)
+        self.get_logger().info("Data received!")
+
+    def get_states(self):
+        rclpy.spin_once(self)
+        time.sleep(0.1)
+        return self.position, self.orientation, self.velocities
+
+
+class CmdVelPublisher(Node):
+    def __init__(self):
+        super().__init__('cmd_vel_publisher')
+        self.cmd_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.pub = Twist()
+
+    def publish_cmd(self, v, w):
+        self.pub.linear.x = v
+        self.pub.angular.z = w
+        self.cmd_publisher.publish(self.pub)
+        self.get_logger().info("cmd published!")
 
 
 def main():
+    rclpy.init()
+    laser_node = LaserSubscriber()
+    odom_node = OdomSubscriber()
+    cmd_publisher = CmdVelPublisher()
     # Define time step
     dt = 0.2
     # Define prediction horizon
     N = 20
+    # Define costmap size and resolution
+    costmap_size = 2
+    resolution = 0.5
     # Define initial state
     x0 = np.array([0, 0, 0])
     # Define final state
-    xf = np.array([10, 10, 0])
+    xf = np.array([5, 5, 0])
     # Create an instance of the MPC class
-    mpc_planner = Mpc(dt, N)
-    count = 0
+    mpc_planner = Mpc(dt, N, cost_factor=1.0, costmap_size=costmap_size, resolution=resolution)
     x_pos = []
     u0 = np.zeros((mpc_planner.n_controls, mpc_planner.N))
-    while np.linalg.norm(x0 - xf) > 0.2 and count < 500:
-        x, u = mpc_planner.perform_mpc(u0=u0, initial_state=x0, final_state=xf)
-        x0 = x[:, 1]
+    obstacles_x = np.ones(int((costmap_size * 2) / resolution) * 2) * 100
+    obstacles_y = np.ones(int((costmap_size * 2) / resolution) * 2) * 100
+    count = 0
+    while np.linalg.norm(x0 - xf) > 0.2 and count < 1000:
+        scan_data, angles = laser_node.get_scan()
+        pos, ori, velocity = odom_node.get_states()
+        if scan_data is None:
+            continue
+
+        occ_grid = 1 - convert_laser_scan_to_occupancy_grid(scan_data, angles, resolution, costmap_size * 2)
+        occ_grid = np.rot90(occ_grid, k=2)
+        x, y = convert_to_map_coordinates(occ_grid=occ_grid, map_resolution=resolution)
+        obstacles_indices = np.where(occ_grid == 0)
+        obs_x, obs_y = x[obstacles_indices], y[obstacles_indices]
+        obstacle_array = np.array([obs_x, obs_y])
+        rotated_obstacle = rotate_coordinates(obstacle_array, ori[2])
+        rotated_obstacle[0, :] += pos[0]
+        rotated_obstacle[1, :] += pos[1]
+
+        y_obs = rotated_obstacle[1, :]
+        x_obs = rotated_obstacle[0, :]
+        try:
+            x_obs_array = obstacles_x * x_obs[0]
+            # x_obs_array = x_obs_array.ravel()
+            x_obs_array[:len(x_obs)] = x_obs
+            # x_obs_array = np.reshape(x_obs_array, obstacles_x.shape)
+            y_obs_array = obstacles_y * y_obs[0]
+            # y_obs_array = y_obs_array.ravel()
+            y_obs_array[:len(y_obs)] = y_obs
+            # y_obs_array = np.reshape(y_obs_array, obstacles_y.shape)
+        except IndexError as e:
+            print(e, "No obstacles")
+            x_obs_array = obstacles_x
+            y_obs_array = obstacles_y
+        x0 = casadi.vertcat(pos[0], pos[1], ori[2])
+        x, u = mpc_planner.perform_mpc(u0=u0, initial_state=x0, final_state=xf, obstacles_x=x_obs_array,
+                                       obstacles_y=y_obs_array)
+        # x0 = x[:, 1]
         u0 = np.concatenate((u[:, 1:], u[:, -1].reshape(2, 1)), axis=1)
         count += 1
         x_pos.append(x0)
         print(x0)
+        cmd_publisher.publish_cmd(u[0, 0], u[1, 0])
+    cmd_publisher.publish_cmd(0.0, 0.0)
 
     x_pos = np.array(x_pos)
     plt.plot(x_pos[:, 0], x_pos[:, 1])
